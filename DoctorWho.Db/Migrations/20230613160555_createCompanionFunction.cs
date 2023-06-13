5eec6d8d506e040e95d251b13f400ecf672b6cd9
Shahd <shahdlubb@gmail.com>
2023-06-13 19:14:41 +0300

    add migration to create Companion by episodeId function

---
+++ DoctorWho.Db/Migrations/20230613160555_createCompanionFunction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Db.Migrations
+{
+    public partial class createCompanionFunction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(SQLStatments.CreateCompanionNameFunction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION  fnCompanions");
+        }
+    }
+}
---
+++ DoctorWho.Db/SQLStatments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db
+{
+    public class SQLStatments
+    {
+       public static string CreateCompanionNameFunction = @"CREATE FUNCTION fnCompanions (@EpisodeId INT)
+                                                            RETURNS VARCHAR(50) AS
+                                                            BEGIN
+                                                               DECLARE @CompanionName VARCHAR(50);
+	                                                           SELECT @CompanionName=[CompanionName]
+	                                                           FROM CompanionEpisode Episodes JOIN  Companions Companions ON Episodes.CompanionsCompanionId=Companions.CompanionId
+	                                                           WHERE Episodes.EpisodeId=@EpisodeId;
+                                                               return @CompanionName
+                                                               END ;";
+
+       }
+}
